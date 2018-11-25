cd9bc647a73e26d0f62afb946884cba5dcd59f43
Football123 <floris.foet@gmail.com>
2018-11-25 14:35:47 +0100

    CSS/HTML Jazz-pagina aangepast + Model aangemaakt voor tonen van informatie over Jazz-events

---
+++ HaarlemFilmFestival/HaarlemFilmFestival/Models/JazzProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HaarlemFilmFestival.Models
+{
+    public class JazzProgram
+    {
+        public Jazz Jazz { get; set; }
+        public Event Event { get; set; }
+        public Location Location { get; set; }
+    }
+}
