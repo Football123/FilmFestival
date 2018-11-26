f2e9a6fb89e47d50935f8c61b6184c2dea995d4f
Football123 <floris.foet@gmail.com>
2018-11-26 16:50:58 +0100

    Nieuw ViewModel aangemaakt + Aparte folder

---
+++ HaarlemFilmFestival/HaarlemFilmFestival/ViewModels/JazzViewModel.cs
@@ -0,0 +1,18 @@
+﻿using HaarlemFilmFestival.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HaarlemFilmFestival.ViewModels
+{
+    public class JazzViewModel
+    {
+        public Event Begintijd { get; set; }
+        public Event Eindtijd { get; set; }
+        public Location Naam { get; set; }
+        public Jazz Rang { get; set; }
+        public Jazz RangPrijs { get; set; }
+        public Jazz Band { get; set; }
+    }
+}
