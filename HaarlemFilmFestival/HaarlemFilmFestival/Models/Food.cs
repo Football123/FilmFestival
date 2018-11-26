789b0c9fc75628507f9a2c2285e0868a8bf037c5
Rigo Pourier <544254@student.inholland.nl>
2018-11-26 16:31:46 +0100

    Food HTML/CSS aangepast

--- HaarlemFilmFestival/HaarlemFilmFestival/Models/Food.cs
+++ HaarlemFilmFestival/HaarlemFilmFestival/Models/Food.cs
@@ -9,8 +9,8 @@ namespace HaarlemFilmFestival.Models
     {
         public int FoodId { get; set; }
         public string Restaurant { get; set; }
-        public string Rang { get; set; }
-        public int Rangprijs { get; set; }
+        public int Prijs_Adult { get; set; }
+        public int Prijs_Child { get; set; }
         public string Type { get; set; }
         public int EventId { get; set; }
     }
