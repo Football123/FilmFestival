92f303efef9d047c234aad2a9a4621c5b6b82de0
Football123 <floris.foet@gmail.com>
2018-12-03 14:15:43 +0100

    Model aangepast Final

--- HaarlemFilmFestival/HaarlemFilmFestival/Models/Event.cs
+++ HaarlemFilmFestival/HaarlemFilmFestival/Models/Event.cs
@@ -9,7 +9,7 @@ namespace HaarlemFilmFestival.Models
     {
         public int Id { get; set; }
         public DateTime StartTime { get; set; }
-        public DateTime? EindTime { get; set; }
+        public DateTime? EndTime { get; set; }
         public double Price { get; set; }
         public double? Discount { get; set;}
         public int? Capacity { get; set;}
