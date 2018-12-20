a4a5c45cbef4d1d82d0d497de3fffc37fdba68fe
kayleighrem <kayleighrem@live.nl>
2018-12-20 15:19:04 +0100

    css & historic aangepast

--- HaarlemFilmFestival/HaarlemFilmFestival/Controllers/HistoricsController.cs
+++ HaarlemFilmFestival/HaarlemFilmFestival/Controllers/HistoricsController.cs
@@ -14,10 +14,11 @@ namespace HaarlemFilmFestival.Controllers
     public class HistoricsController : Controller
     {
         private IHistoricRepository historicrepository = new HistoricRepository();
-        private HistoricViewModel viewmodel = new HistoricViewModel();
+        public HistoricViewModel viewmodel = new HistoricViewModel();
 
         public ActionResult Index()
         {
+            viewmodel = FillViewModel();
             return View(viewmodel);
         }
 
@@ -25,6 +26,59 @@ namespace HaarlemFilmFestival.Controllers
         public ActionResult Index(OrderRecord orderrecords)
         {
             return View();
+        }
+
+        public HistoricViewModel FillViewModel()
+        {
+            viewmodel.Historics = historicrepository.GetHistoricEvents();
+            viewmodel.languages = GetLanguages();
+            viewmodel.Stops = historicrepository.GetStops();
+            viewmodel.eventsLeft = GetAvailableEvents();
+            viewmodel.historicsLeft = getHistoricsLeft();
+            viewmodel.days = viewmodel.getDays(viewmodel.eventsLeft);
+            viewmodel.times = viewmodel.getStartTime(viewmodel.eventsLeft);
+            return viewmodel;
+        }
+
+        public IEnumerable<Historic> getHistoricsLeft()
+        {
+            List<Historic> left = new List<Historic>();
+            foreach (Historic historic in viewmodel.Historics)
+            {
+                foreach (Event Event in GetAvailableEvents())
+                {
+                    if (historic.Id.Equals(Event.Id))
+                        left.Add(historic);
+                }
+            }
+            return left;
+        }
+
+        public IEnumerable<Language> GetLanguages()
+        {
+            List<Language> languages = new List<Language>();
+            foreach (Historic historic in getHistoricsLeft())
+            {
+                if (!languages.Contains(historic.Languages))
+                    languages.Add(historic.Languages);
+            }
+            return languages;
+        }
+
+        public IEnumerable<Event> GetAvailableEvents()
+        {
+            IEnumerable<OrderRecord> ordered;
+            ordered = historicrepository.GetOrderedEvents();
+            List<Event> Events = new List<Event>();
+            foreach (Event Event in viewmodel.Historics)
+            {
+                int Count = 0;
+                foreach (OrderRecord orderrecord in ordered)
+                    Count = Count + orderrecord.RecordAmount;
+                if (Count < Event.Capacity)
+                    Events.Add(Event);
+            }
+            return Events;
         }
     }
 }
--- HaarlemFilmFestival/HaarlemFilmFestival/Models/ViewModels/HistoricViewModel.cs
+++ HaarlemFilmFestival/HaarlemFilmFestival/Models/ViewModels/HistoricViewModel.cs
@@ -10,60 +10,9 @@ namespace HaarlemFilmFestival.Models
 {
     public class HistoricViewModel : EventViewModel
     {
-        private IHistoricRepository historicrepository = new HistoricRepository();
-
         public HistoricViewModel()
         {
-            this.Stops = historicrepository.GetStops();
-            this.Historics = historicrepository.GetHistoricEvents();
-            this.eventsLeft = AvailableEvents();
-            this.historicsLeft = getHistoricsLeft();
-            this.days = getDays(eventsLeft);
-            this.times = getStartTime(eventsLeft);
-            this.languages = getlanguages();
         }
-
-        private IEnumerable<Historic> getHistoricsLeft()
-        {
-            List<Historic> left = new List<Historic>();
-            foreach (Historic historic in Historics)
-            {
-                foreach (Event Event in eventsLeft)
-                {
-                    if (historic.Id.Equals(Event.Id))
-                        left.Add(historic);
-                }
-            }
-            return left;
-        }
-
-        private IEnumerable<Event> AvailableEvents()
-        {
-            IEnumerable<OrderRecord> ordered;
-            ordered = historicrepository.GetOrderedEvents();
-            List<Event> Events = new List<Event>();
-            foreach (Event Event in this.Historics)
-            {
-                int Count = 0;
-                foreach (OrderRecord orderrecord in ordered)
-                    Count = Count + orderrecord.RecordAmount;
-                if (Count < Event.Capacity)
-                    Events.Add(Event);
-            }
-            return Events;
-        }
-
-        private IEnumerable<Language> getlanguages()
-        {
-            List<Language> languages = new List<Language>();
-            foreach (Historic historic in historicsLeft)
-            {
-                if (!languages.Contains(historic.Languages))
-                    languages.Add(historic.Languages);
-            }
-            return languages;
-        }
-
 
         public IEnumerable<Language> languages { get; set; }
         public IEnumerable<HistoricStop> Stops { get; set; }
