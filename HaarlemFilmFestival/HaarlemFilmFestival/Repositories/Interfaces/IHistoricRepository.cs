f5a757c79fd7e939dbe793e748e2d94a7dc252d1
kayleighrem <kayleighrem@live.nl>
2019-01-09 14:22:37 +0100

    van alles aangepast en GOOGLE MAPS GEÏMPLEMENTEERD (THANKS ARIF <3 )

--- HaarlemFilmFestival/HaarlemFilmFestival/Repositories/Interfaces/IHistoricRepository.cs
+++ HaarlemFilmFestival/HaarlemFilmFestival/Repositories/Interfaces/IHistoricRepository.cs
@@ -10,6 +10,7 @@ namespace HaarlemFilmFestival.Repositories
 {
     interface IHistoricRepository
     {
+        IEnumerable<Historic> GetPerDayAndTime(DateTime daytime);
         IEnumerable<Historic> GetHistoricPerTime(DateTime time);
         IEnumerable<Historic> GetHistoricPerDay(DateTime day);
         IEnumerable<HistoricStop> GetStops();
