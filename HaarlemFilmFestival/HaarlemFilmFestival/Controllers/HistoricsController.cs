19798c9462fac6e9c86f8f4746354250a7fa044a
kayleighrem <kayleighrem@live.nl>
2019-01-16 16:17:54 +0100

    Update HistoricsController.cs

--- HaarlemFilmFestival/HaarlemFilmFestival/Controllers/HistoricsController.cs
+++ HaarlemFilmFestival/HaarlemFilmFestival/Controllers/HistoricsController.cs
@@ -41,6 +41,7 @@ namespace HaarlemFilmFestival.Controllers
                 orderrecords.TicketType = TicketType.Single;
             Session["Orders"] = order;
 
+            viewmodel = FillViewModel();
             return View(viewmodel);
         }
 
